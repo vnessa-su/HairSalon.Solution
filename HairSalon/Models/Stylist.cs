2c69aee726ca163177564579d87983e6dabab74d
Vanessa Su <vnessa.su@gmail.com>
2021-05-21 12:07:27 -0400

    :hammer: Add properties to Stylist model

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,7 +1,19 @@
+using System.Collections.Generic;
+
 namespace HairSalon.Models
 {
   public class Stylist
   {
-    //properties
+    public int StylistId {get; set;}
+    public string Name {get; set;}
+    public string PhoneNumber {get; set;}
+    public string Email {get; set;}
+    public string Address {get; set;}
+    public virtual ICollection<Client> Clients {get; set;}
+
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
   }
 }
