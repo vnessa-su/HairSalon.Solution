c0cd2f816a38107980c89c46c8549ece6b67c287
Vanessa Su <vnessa.su@gmail.com>
2021-05-21 12:58:21 -0400

    :hammer: Add display decorator to Stylist PhoneNumber property

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -6,10 +6,12 @@ namespace HairSalon.Models
   {
     public int StylistId {get; set;}
     public string Name {get; set;}
-    public string PhoneNumber {get; set;}
     public string Email {get; set;}
     public string Address {get; set;}
     public virtual ICollection<Client> Clients {get; set;}
+
+    [Display(Name = "Phone Number")]
+    public string PhoneNumber {get; set;}
 
     public Stylist()
     {
