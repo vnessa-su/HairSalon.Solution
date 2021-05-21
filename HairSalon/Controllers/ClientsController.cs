f69c52acf1fb9cf1a8c389d071a1179e4534031c
Vanessa Su <vnessa.su@gmail.com>
2021-05-21 12:33:51 -0400

    :hammer: Add routing for Index and Create

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> clientList = _db.Clients.Include(Client => client.Stylist).ToList();
+      return View(clientList);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      return View();
+    }
+
+    public ActionResult Edit(int id)
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Edit()
+    {
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      return View();
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      return RedirectToAction("Index");
+    }
+  }
+}
