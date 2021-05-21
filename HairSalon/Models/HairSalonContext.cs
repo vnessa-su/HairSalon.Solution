dfbed8f88fd09b439ce14d98668efab792769423
Vanessa Su <vnessa.su@gmail.com>
2021-05-21 11:39:16 -0400

    :hammer: Include Clients and Stylists tables in Context

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -4,7 +4,8 @@ namespace HairSalon.Models
 {
   public class HairSalonContext : DbContext
   {
-    public virtual DbSet<Model> Models { get; set; }
+    public virtual DbSet<Stylist> Stylists { get; set; }
+    public virtual DbSet<Client> Clients { get; set; }
 
     public HairSalonContext(DbContextOptions options) : base(options) { }
 
